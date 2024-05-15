afa2b7da251a74f9f7c5ea858b5442ff1ccc4453
Achala Punsara <achala.magallagoda@gmail.com>
2024-05-15 21:17:25 +0530

    First 2 Java Programs

---
+++ DataType.java
@@ -0,0 +1,21 @@
+public class DataType {
+    public static void main(String[] args){
+        System.out.println("There are 8 types of primitive data types in java\n" +
+                "They are like below : \n" +
+                "1. Boolean\n" +
+                "2. Byte\n" +
+                "3. Short\n" +
+                "4. Integer\n" +
+                "5. Long\n" +
+                "6. Float\n" +
+                "7. Double \n" +
+                "8. Char");
+
+        boolean x = true ;
+        System.out.println("Boolean : "+ true);
+
+        byte y = 4;
+        System.out.println(y);
+
+    }
+}
---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+/** Doc Comment */
+
+/* Multi Line comment*/
+
+// Single line comment
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello JAVA");
+    }
+}
